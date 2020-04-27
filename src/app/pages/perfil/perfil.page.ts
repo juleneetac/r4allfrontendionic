ae1d8b3bb3b29d7e8d5c247e69aa27cbbe85c9e6
13erMVP <anibalbarca13@gmail.com>
2020-04-27 01:07:16 +0100

    problema undefined

--- src/app/pages/perfil/perfil.page.ts
+++ src/app/pages/perfil/perfil.page.ts
@@ -9,6 +9,7 @@ import {ServicePerfilService} from "../../services/servicePerfil/service-perfil.
 import { StringMap } from '@angular/compiler/src/compiler_facade_interface';
 import { Message } from '@angular/compiler/src/i18n/i18n_ast';
 import { Observable } from "rxjs";
+import { strictEqual } from 'assert';
 
 interface HtmlInputEvent extends Event {
   target: HTMLInputElement & EventTarget;
@@ -34,6 +35,8 @@ export class PerfilPage implements OnInit {
   usernombre = localStorage.getItem("username");
   passold = localStorage.getItem("password");
 
+  id: string;
+  
 
 file: File;
 photoSelected: string | ArrayBuffer
@@ -54,29 +57,42 @@ photoSelected: string | ArrayBuffer
 
 /*   contraseña1, contraseña2, mail, sexo, ubicacion, edad, experiencia
  */
+  getuserbyid(){
+    console.log(this.usernombre);
+    this.ServicePerfilService.getuserbyid(this.usernombre)
+    .subscribe(
+      (res) => {
+        this.id = JSON.stringify(res);
+        console.log(res);
+      },
+      (err) => {
+        console.log(err);
+      });
 
+  }
 
-  updatePerfil (contraseña1: HTMLInputElement, contraseña2: HTMLInputElement, mail: HTMLInputElement, sexo: HTMLInputElement, ubicacion:HTMLInputElement, edad: HTMLInputElement, experiencia: HTMLInputElement){
+  updatePerfil (contrasena1: HTMLInputElement, contrasena2: HTMLInputElement, mail: HTMLInputElement, sexo: HTMLInputElement, ubicacion:HTMLInputElement, edad: HTMLInputElement, experiencia: HTMLInputElement, id){
   
-  console.log(contraseña1.value);
-  console.log(contraseña2.value);
+  console.log(contrasena1.value);
+  console.log(contrasena2.value);
   console.log(mail.value);
   console.log(sexo.value);
   console.log(ubicacion.value);
   console.log(edad.value);
   console.log(experiencia.value);
+  console.log(id);
   
-  
-   if(contraseña1.value==this.passold) //la que está al iniciar sesión
+ 
+   if(contrasena1.value == this.passold) //la que está al iniciar sesión
     {
-       this.ServicePerfilService.update(contraseña2.value, mail.value, sexo.value, ubicacion.value, edad.value, experiencia.value, this.file)
-      .subscribe(
+       this.ServicePerfilService.update(contrasena2.value, mail.value, sexo.value, ubicacion.value, edad.value, experiencia.value, this.file, id)
+        .subscribe(
         (res) => {
           console.log(res);
         },
         (err) => {
           console.log(err);
-        });
+        });  
        
     }
   else{
--- src/app/services/servicePerfil/service-perfil.service.ts
+++ src/app/services/servicePerfil/service-perfil.service.ts
@@ -7,6 +7,7 @@ import { Observable } from "rxjs";
 })
 export class ServicePerfilService {
 
+  id: string;
 
   usernombre = localStorage.getItem("username");
   passold = localStorage.getItem("password");
@@ -15,19 +16,25 @@ export class ServicePerfilService {
   constructor(private http: HttpClient) { }
 
 
-update (contraseña2: string, mail: string, sexo: string, ubicacion:string, edad: string, valoracion: string, photo:File){
 
-  const BASE_URI = "http://localhost:7000/perfil/update"
+getuserbyid(username){
+return this.http.get("http://localhost:7000/usr/getidofuser/"+`${username}`)
+}
+
+update (contraseña2: string, mail: string, sexo: string, ubicacion:string, edad: string, valoracion: string, photo:File, id: string){
+
+
   const fd = new FormData();
+  const Id = id;
   
     fd.append('username',this.usernombre);
     fd.append('mail',mail);
     fd.append('password',contraseña2);
     fd.append('sexo', sexo);
-    fd.append('image', photo);
+    fd.append('rutaimagen', photo);
     fd.append('ubicacion',ubicacion);
     fd.append('edad',edad);
     fd.append('valoracion',valoracion);
-  
-
+    return this.http.put("http://localhost:7000/usr/update/"+`${Id}`,fd)
+  }
 }
