71b5ef069c161bbb0ddd781aff5393cebe72dc3e
Alexlopez52 <55534400+Alexlopez52@users.noreply.github.com>
2020-05-21 12:40:27 +0200

    Arreglo error ambiente

--- src/app/services/ambiente.ts
+++ src/app/services/ambiente.ts
@@ -20,20 +20,20 @@ path:string;
     
     public prepareAmbiente(){
       
-    this.urlUsuario = this.path +'/usr';  //deben ser las mismas rutas creadas
-    this.urlTorneo = this.path + '/trn';  //en el backend
-    this.urlPartida =  this.path +'/prd';
-    this.urlParticipantest =  this.path +'/prantes';
-    this.urlMensaje =  this.path +'/msg';
-    this.urlChat = this.path + '/cht';
+    this.urlUsuario = this.path +'usr';  //deben ser las mismas rutas creadas
+    this.urlTorneo = this.path + 'trn';  //en el backend
+    this.urlPartida =  this.path +'prd';
+    this.urlParticipantest =  this.path +'prantes';
+    this.urlMensaje =  this.path +'msg';
+    this.urlChat = this.path + 'cht';
     }
     public configureRemoteServer() {
       console.log ("estas en " + location.hostname)
       if (location.hostname === '147.83.7.156') {
-        this.path = 'https://147.83.7.156:8080';
+        this.path = 'https://147.83.7.156:8080/';
       }
       else if(location.hostname==='localhost'){
-        this.path = 'https://localhost:7000'
+        this.path = 'https://localhost:7000/'
       }    
     }
 }
