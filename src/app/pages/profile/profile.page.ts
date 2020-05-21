fa85c4cb47e67208a2d28cea9dd4656f2ae7f481
Alexlopez52 <55534400+Alexlopez52@users.noreply.github.com>
2020-05-21 12:25:59 +0200

    Cambios ambiente

--- src/app/pages/profile/profile.page.ts
+++ src/app/pages/profile/profile.page.ts
@@ -9,11 +9,15 @@ import { Modelusuario } from 'src/app/models/modelUsusario/modelusuario';
   styleUrls: ['./profile.page.scss'],
 })
 export class ProfilePage implements OnInit {
-  
+  ambiente: Ambiente; 
+  path;
   constructor(
     public auth: AuthService,  //se puede quitar creo
     private storage: StorageComponent
-  ) { }
+  ) { 
+    this.ambiente = new Ambiente();
+      this.path=this.ambiente.path; 
+    }
 
   localperfil: Modelusuario;
     
