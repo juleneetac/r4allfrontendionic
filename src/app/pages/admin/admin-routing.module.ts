bd7ff05b19bcae4b7b74899f33989b5e3dc5834a
Alexlopez52 <alexlopeztbj@gmail.com>
2020-06-30 09:30:50 +0200

    guard en admin

--- src/app/pages/admin/admin-routing.module.ts
+++ src/app/pages/admin/admin-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
+import { AuthGuard } from 'src/app/services/serviceGuard/auth.guard';
 import { AdminPage } from './admin.page';
 
 const routes: Routes = [
   {
     path: '',
-    component: AdminPage
+    component: AdminPage,
+    canActivate: [AuthGuard]
   }
 ];
 
